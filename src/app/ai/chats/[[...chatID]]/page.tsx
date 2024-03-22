13e9513481507a485a82f3b5b45c61367bb72072
yikazhu <yikazhu@tencent.com>
2024-03-23 00:54:45 +0800

    feat: 更新 layout  实现局部路由刷新

--- src/app/ai/chats/[[...chatID]]/page.tsx
+++ src/app/ai/chats/[[...chatID]]/page.tsx
@@ -1,31 +1,10 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from '@/components/ui/resizable';
-import List from './List';
-import { Separator } from '@/components/ui/separator';
-import Conversation from './Conversation';
-import { NewChat } from './NewChat';
-import { EventProvider } from './EventProvider';
+import Conversation from '../Conversation';
 
 // eslint-disable-next-line @next/next/no-async-client-component
-export default function Chat() {
+export default function Chat({ children }: { children: React.ReactNode }) {
   return (
-    <EventProvider>
-      <ResizablePanelGroup direction="horizontal" className="border">
-        <ResizablePanel defaultSize={10} minSize={10} maxSize={30}>
-          <div className="p-3">
-            <NewChat />
-            <Separator className="my-3" />
-          </div>
-          <List />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={90}>
-          <Conversation />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </EventProvider>
+    <>
+      <Conversation />
+    </>
   );
 }
---
+++ src/app/ai/chats/layout.tsx
@@ -0,0 +1,32 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable';
+import List from './List';
+import { Separator } from '@/components/ui/separator';
+import Conversation from './Conversation';
+import { NewChat } from './NewChat';
+import { EventProvider } from './EventProvider';
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <EventProvider>
+      <ResizablePanelGroup direction="horizontal" className="border">
+        <ResizablePanel defaultSize={10} minSize={10} maxSize={30}>
+          <div className="p-3">
+            <NewChat />
+            <Separator className="my-3" />
+          </div>
+          <List />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={90}>{children}</ResizablePanel>
+      </ResizablePanelGroup>
+    </EventProvider>
+  );
+}
